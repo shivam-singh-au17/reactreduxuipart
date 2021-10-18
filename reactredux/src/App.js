b3ebcb77a85dd69e2facaff4c1f0af478c48d035
Shivam Singh <shivamsingh4458@gmail.com>
2021-10-18 13:45:48 +0530

    file update

---
+++ reactredux/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+// import AddTask from './components/ConfiguringStore/AddTask';
+import Routes from './Routes/Routes';
+function App() {
+  return (
+    <>
+      <Routes/>
+    </>
+  );
+}
+
+export default App;
+
---
+++ reactredux/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+// import { store } from './AllState/ReduxIntroduction/store';
+// import { store } from './AllState/ReactRedux/store';
+import { store } from './AllState/ConfiguringStore/store';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
