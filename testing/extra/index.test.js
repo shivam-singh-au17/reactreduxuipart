1177861d4858913e2a652116dd0d48b890a80434
Shivam Singh <shivamsingh4458@gmail.com>
2021-10-23 16:26:33 +0530

    adding some updates

---
+++ testing/extra/index.test.js
@@ -0,0 +1,9 @@
+var sum = require("../Testing/index.js");
+
+describe("test case for sum function", function () {
+    
+    test('should sum function defined', () => {
+        expect(sum).toBeDefined()
+    })
+    
+})
